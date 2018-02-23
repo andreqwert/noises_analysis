4a398642cc5a44e6bd1b2d59f2e6719e20929661
Andrey <timebird@yandex.ru>
2018-02-23 07:00:20 +0300

    Add files via upload

--- noises_classification.py
+++ noises_classification.py
@@ -38,15 +38,15 @@ def decision_trees_classifier(X, y, cv):
     tree = DecisionTreeClassifier(random_state=17)   # объект модели
     tree.fit(X_train, y_train)
     tree_pred = tree.predict(X_test)
-    print('Decision trees: ', accuracy_score(y_test, tree_pred)) # 0.693181818182
+    print('Decision trees: ', accuracy_score(y_test, tree_pred)) # 0.625
 
 
     # Подкручиваем параметры: настройка максимальной глубины дерева
     tree_params = {'max_depth': range(1, 11)}
     tree_grid = GridSearchCV(tree, tree_params, cv=5, n_jobs=-1) # n_jobs=-1 <-- задействовать все ядра процессора
     tree_grid.fit(X_train, y_train)
-    print('Decision trees, GridSearchCV-params: ', tree_grid.best_params_) # {'max_depth': 8}
-    print('Decision trees, GridSearchCV-score: ', tree_grid.best_score_) # 0.655172413793
+    print('Decision trees, GridSearchCV-params: ', tree_grid.best_params_) # {'max_depth': 6}
+    print('Decision trees, GridSearchCV-score: ', tree_grid.best_score_) # 0.660098522167
     print('Accuracy: ', accuracy_score(y_test, tree_grid.predict(X_test)))
     print('----------------\n')
 
@@ -66,9 +66,9 @@ def knn_classifier(X, y, cv):
     knn_params = {'knn__n_neighbors':range(1, 10)}
     knn_grid = GridSearchCV(knn_pipe, knn_params, cv=5, n_jobs=-1)
     knn_grid.fit(X_train, y_train)
-    print('kNN, GridSearchCV-params: ', knn_grid.best_params_)
-    print('kNN, GridSearchCV-score: ', knn_grid.best_score_)
-    print('Accuracy: ', accuracy_score(y_test, knn_grid.predict(X_test)))
+    print('kNN, GridSearchCV-params: ', knn_grid.best_params_) # {'knn__n_neighbors': 4}
+    print('kNN, GridSearchCV-score: ', knn_grid.best_score_) # 0.738916256158
+    print('Accuracy: ', accuracy_score(y_test, knn_grid.predict(X_test))) # 0.659090909091
     print('----------------\n')
 
 
@@ -84,9 +84,9 @@ def RFClassifier(X, y, cv):
     forest_params = {'max_depth': range(1, 11)}
     forest_grid = GridSearchCV(forest, forest_params, cv=5, n_jobs=-1)
     forest_grid.fit(X_train, y_train)
-    print('RandomForest, GridSearchCV-params: ', forest_grid.best_params_)
-    print('RandomForest, GridSearchCV-score: ', forest_grid.best_score_)
-    print('Accuracy: ', accuracy_score(y_test, forest_grid.predict(X_test)))
+    print('RandomForest, GridSearchCV-params: ', forest_grid.best_params_) #  {'max_depth': 9}
+    print('RandomForest, GridSearchCV-score: ', forest_grid.best_score_) # 0.71921182266
+    print('Accuracy: ', accuracy_score(y_test, forest_grid.predict(X_test))) # 0.784090909091
     print('----------------\n')
 
 
@@ -96,5 +96,7 @@ AdaBoost_classifier(X, y, cv)
 RFClassifier(X, y, cv)
 decision_trees_classifier(X, y, cv)
 knn_classifier(X, y, cv)
+
+
 
 
