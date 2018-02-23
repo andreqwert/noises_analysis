1f06f8d269134b1591eee30e736aaa5ed44e9191
Andrey <timebird@yandex.ru>
2018-02-23 06:52:50 +0300

    Update noises_classification.py

--- noises_classification.py
+++ noises_classification.py
@@ -34,42 +34,42 @@ def AdaBoost_classifier(X, y, cv):
 
 def decision_trees_classifier(X, y, cv):
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=17)
-	tree = DecisionTreeClassifier(random_state=17)   # объект модели
-	tree.fit(X_train, y_train)
-	tree_pred = tree.predict(X_test)
-	print('Decision trees: ', accuracy_score(y_test, tree_pred)) # 0.693181818182
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=17)
+    tree = DecisionTreeClassifier(random_state=17)   # объект модели
+    tree.fit(X_train, y_train)
+    tree_pred = tree.predict(X_test)
+    print('Decision trees: ', accuracy_score(y_test, tree_pred)) # 0.693181818182
 
 
-	# Подкручиваем параметры: настройка максимальной глубины дерева
-	tree_params = {'max_depth': range(1, 11)}
-	tree_grid = GridSearchCV(tree, tree_params, cv=5, n_jobs=-1) # n_jobs=-1 <-- задействовать все ядра процессора
-	tree_grid.fit(X_train, y_train)
-	print('Decision trees, GridSearchCV-params: ', tree_grid.best_params_) # {'max_depth': 8}
-	print('Decision trees, GridSearchCV-score: ', tree_grid.best_score_) # 0.655172413793
-	print('Accuracy: ', accuracy_score(y_test, tree_grid.predict(X_test)))
-	print('----------------\n')
+    # Подкручиваем параметры: настройка максимальной глубины дерева
+    tree_params = {'max_depth': range(1, 11)}
+    tree_grid = GridSearchCV(tree, tree_params, cv=5, n_jobs=-1) # n_jobs=-1 <-- задействовать все ядра процессора
+    tree_grid.fit(X_train, y_train)
+    print('Decision trees, GridSearchCV-params: ', tree_grid.best_params_) # {'max_depth': 8}
+    print('Decision trees, GridSearchCV-score: ', tree_grid.best_score_) # 0.655172413793
+    print('Accuracy: ', accuracy_score(y_test, tree_grid.predict(X_test)))
+    print('----------------\n')
 
 
 
 def knn_classifier(X, y, cv):
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-	knn = KNeighborsClassifier()   # объект модели
-	knn.fit(X_train, y_train)
-	knn_pred = knn.predict(X_test)
-	print('kNN: ', accuracy_score(y_test, knn_pred)) # 0.534090909091
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    knn = KNeighborsClassifier()   # объект модели
+    knn.fit(X_train, y_train)
+    knn_pred = knn.predict(X_test)
+    print('kNN: ', accuracy_score(y_test, knn_pred)) # 0.534090909091
 
 
-	# Попробуем настроить число соседей в алгоритме kNN
-	knn_pipe = Pipeline([('scaler', StandardScaler()), ('knn', KNeighborsClassifier(n_jobs=-1))])
-	knn_params = {'knn__n_neighbors':range(1, 10)}
-	knn_grid = GridSearchCV(knn_pipe, knn_params, cv=5, n_jobs=-1)
-	knn_grid.fit(X_train, y_train)
-	print('kNN, GridSearchCV-params: ', knn_grid.best_params_)
-	print('kNN, GridSearchCV-score: ', knn_grid.best_score_)
-	print('Accuracy: ', accuracy_score(y_test, knn_grid.predict(X_test)))
-	print('----------------\n')
+    # Попробуем настроить число соседей в алгоритме kNN
+    knn_pipe = Pipeline([('scaler', StandardScaler()), ('knn', KNeighborsClassifier(n_jobs=-1))])
+    knn_params = {'knn__n_neighbors':range(1, 10)}
+    knn_grid = GridSearchCV(knn_pipe, knn_params, cv=5, n_jobs=-1)
+    knn_grid.fit(X_train, y_train)
+    print('kNN, GridSearchCV-params: ', knn_grid.best_params_)
+    print('kNN, GridSearchCV-score: ', knn_grid.best_score_)
+    print('Accuracy: ', accuracy_score(y_test, knn_grid.predict(X_test)))
+    print('----------------\n')
 
 
 
@@ -96,7 +96,5 @@ AdaBoost_classifier(X, y, cv)
 RFClassifier(X, y, cv)
 decision_trees_classifier(X, y, cv)
 knn_classifier(X, y, cv)
-
-
 
 
